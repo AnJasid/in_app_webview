0d084ef4129e8b8d32e112a9aa36c9e7ef1d8d5f
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-01 14:42:46 +0800

    implement webview in the app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:in_app_webview/webview_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void _goToWebView(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const WebViewPage(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +21,10 @@ class HomePage extends StatelessWidget {
         ),
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () => _goToWebView(context),
                 child: const Text('Start'),
               ),
             ],
--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
 
   @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  double _progress = 0;
+
+  late InAppWebViewController inAppWebViewController;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return WillPopScope(
+      onWillPop: () async {
+        var isLastPage = await inAppWebViewController.canGoBack();
+
+        if (isLastPage) {
+          inAppWebViewController.goBack();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: WebUri(''),
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                inAppWebViewController = controller;
+              },
+              onProgressChanged:
+                  (InAppWebViewController controller, int progress) {
+                setState(() {
+                  _progress = progress / 100;
+                });
+              },
+            ),
+            _progress < 1
+                ? Container(
+                    child: LinearProgressIndicator(
+                      value: _progress,
+                    ),
+                  )
+                : const SizedBox(),
+          ],
+        ),
+      ),
+    );
   }
 }
